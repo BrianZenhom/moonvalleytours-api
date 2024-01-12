d7b5f6287433e9d86820feb60e480bd480c2d93e
brianzenhom <brianzenhom@hotmail.com>
2024-01-12 14:11:14 +0100

    Add limit to request body size

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ mongoose.connection.on('disconnected', () => {
 // Middleware
 app.use(cors())
 app.use(cookieParser())
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
 app.use('/api/auth', authRoute)
 app.use('/api/countries', countriesRoute)
