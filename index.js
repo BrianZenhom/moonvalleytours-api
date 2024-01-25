8133b688c11f69bdd01a10bc0717ce4e01efabda
brianzenhom <brianzenhom@hotmail.com>
2024-01-25 20:09:04 +0100

    Update CORS origin to include localhost:5173

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ mongoose.connection.on('disconnected', () => {
 // Middleware
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
     credentials: true,
   })
 )
