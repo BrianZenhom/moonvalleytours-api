9624f0489220bd1bc90d786ee801553c96edc659
brianzenhom <brianzenhom@hotmail.com>
2024-01-25 20:05:54 +0100

    Add CORS configuration to allow requests from localhost:3000

--- index.js
+++ index.js
@@ -25,7 +25,12 @@ mongoose.connection.on('disconnected', () => {
 })
 
 // Middleware
-app.use(cors())
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+)
 app.use(cookieParser())
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true, limit: '50mb' }))
