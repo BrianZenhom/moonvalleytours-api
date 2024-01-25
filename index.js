9be356ede561743f21f9751194e3e7e0c120b48a
brianzenhom <brianzenhom@hotmail.com>
2024-01-25 20:09:49 +0100

    Add new origin URL to CORS whitelist

--- index.js
+++ index.js
@@ -27,7 +27,11 @@ mongoose.connection.on('disconnected', () => {
 // Middleware
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'http://localhost:5174',
+    ],
     credentials: true,
   })
 )
