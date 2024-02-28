022a294c9f79fa92f5fe05aebbdad74c9f278ec3
brianzenhom <brianzenhom@hotmail.com>
2024-02-28 18:08:48 +0100

    Add email validation and password hashing to User model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose'
+import validator from 'validator'
+import bcrypt from 'bcryptjs'
 
 const UserSchema = new mongoose.Schema(
   {
@@ -7,15 +9,23 @@ const UserSchema = new mongoose.Schema(
       required: [true, 'Please provide your email'],
       unique: true,
       lowercase: true,
+      validate: [validator.isEmail, 'Please provide a valid email'],
     },
     password: {
       type: String,
       required: [true, 'Please provide a password'],
-      minlength: [8, 'Password must be at least 8 characters long'],
+      minlength: 8,
     },
     passwordConfirm: {
       type: String,
       required: [true, 'Please confirm your password'],
+      // This only works on CREATE and SAVE
+      validate: {
+        validator: function (el) {
+          return el === this.password
+        },
+        message: 'Passwords are not the same!',
+      },
     },
     photo: { type: String },
     name: { type: String, required: [true, 'Please tell us your name!'] },
@@ -30,5 +40,18 @@ const UserSchema = new mongoose.Schema(
   },
   { timestamps: true }
 )
+
+UserSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next()
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12)
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined
+
+  next()
+})
 
 export default mongoose.model('User', UserSchema)
