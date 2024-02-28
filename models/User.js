432805c5dd8b4049b670c002bb91c0f61d5d0da7
brianzenhom <brianzenhom@hotmail.com>
2024-02-28 13:46:03 +0100

    Refactor UserSchema in User.js

--- models/User.js
+++ models/User.js
@@ -2,10 +2,24 @@ import mongoose from 'mongoose'
 
 const UserSchema = new mongoose.Schema(
   {
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    name: { type: String },
-    surname: { type: String },
+    email: {
+      type: String,
+      required: [true, 'Please provide your email'],
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide a password'],
+      minlength: [8, 'Password must be at least 8 characters long'],
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, 'Please confirm your password'],
+    },
+    photo: { type: String },
+    name: { type: String, required: [true, 'Please tell us your name!'] },
+    surname: { type: String, required: [true, 'Please tell us your surname'] },
     phone: { type: Number },
     nationality: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
