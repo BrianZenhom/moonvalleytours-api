6df7db6f0489167bac210f07b3c50b70099386cf
brianzenhom <brianzenhom@hotmail.com>
2024-02-28 13:45:56 +0100

    Fix status code and add validation for tour thumbnail and user fields

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,7 +18,7 @@ export const register = async (req, res, next) => {
     })
 
     await newUser.save()
-    res.status(200).send('User has been created.')
+    res.status(201).send('User has been created.')
   } catch (err) {
     next(err)
   }
