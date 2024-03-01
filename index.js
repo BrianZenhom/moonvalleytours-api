4f2888225cc7bf904adbfd2cda23a0601a571ddd
brianzenhom <brianzenhom@hotmail.com>
2024-03-01 14:32:32 +0100

    Refactor error handling in index.js

--- index.js
+++ index.js
@@ -47,20 +47,20 @@ app.use('/api/cities', citiesRoute)
 app.use('/api/countries', countriesRoute)
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
-  })
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`)
+  err.status = 'fail'
+  err.statusCode = 404
+
+  next(err)
 })
 
 app.use((err, req, res, next) => {
-  const errorStatus = err.status || 500
-  const errorMessage = err.message || 'Something went wrong'
-  return res.status(errorStatus).json({
-    success: false,
-    status: errorStatus,
-    message: errorMessage,
-    stack: err.stack,
+  err.statusCode = err.statusCode || 500
+  err.status = err.status || 'error'
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   })
 })
 
