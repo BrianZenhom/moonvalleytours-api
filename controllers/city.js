9b7473489cd1633b62b05da4655677b8a3eef0fe
brianzenhom <brianzenhom@hotmail.com>
2024-03-04 11:30:36 +0100

    Update city creation to use city ID instead of city name

--- controllers/city.js
+++ controllers/city.js
@@ -11,7 +11,7 @@ export const createCity = async (req, res, next) => {
       await Country.findOneAndUpdate(
         { country: countryName },
         {
-          $push: { cities: savedCity.city },
+          $push: { cities: savedCity._id },
         }
       )
     } catch (err) {
