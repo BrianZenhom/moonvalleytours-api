da4a4c2e390cecb9c9ec7d7dfe174720aa04232c
brianzenhom <brianzenhom@hotmail.com>
2024-03-04 16:43:49 +0100

    Refactor error handling in controllers

--- controllers/error.js
+++ controllers/error.js
@@ -1,9 +1,40 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  })
+}
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    })
+
+    // Programming or other unknown error: don't want to leak details
+  } else {
+    // 1) Log Error
+    console.error('ERROR 🪳', err)
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    })
+  }
+}
+
 export default (err, req, res, next) => {
   err.statusCode = err.statusCode || 500
   err.status = err.status || 'error'
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  })
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res)
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res)
+  }
 }
