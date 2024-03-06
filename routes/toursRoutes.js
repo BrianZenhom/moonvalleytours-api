387c64439026ceeb2f2cdc9055a186b97f92c9f9
brianzenhom <brianzenhom@hotmail.com>
2024-03-06 16:59:26 +0100

    Refactor tour routes and controllers

--- routes/toursRoutes.js
+++ routes/toursRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express'
-import {
-  createTour,
-  deleteTour,
-  getAllTour,
-  getTour,
-  getToursInCity,
-  updateTour,
-} from '../controllers/tourController.js'
 import { verifyAdmin } from '../utils/verifyToken.js'
+import {
+  getTour,
+  createTour,
+  getAllTour,
+  updateTour,
+  deleteTour,
+  getToursInCity,
+} from '../controllers/tourController.js'
 import { protect } from '../controllers/authController.js'
 
 const router = express.Router()
@@ -18,8 +18,8 @@ router.get('/:id', getTour)
 router.get('/in/:city', getToursInCity)
 
 // Admin routes
-router.patch('/:id', verifyAdmin, updateTour)
 router.post('/:city', protect, createTour)
+router.patch('/:id', verifyAdmin, updateTour)
 router.delete('/:id/:city', verifyAdmin, deleteTour)
 
 export default router
