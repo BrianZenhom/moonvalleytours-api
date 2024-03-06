d2ca0f380bf3067bff33fde3a355069169777bdd
brianzenhom <brianzenhom@hotmail.com>
2024-03-06 02:31:31 +0100

    Add protect middleware to authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
+import { promisify } from 'util'
 import jwt from 'jsonwebtoken'
 import User from '../models/userModel.js'
+import catchAsync from './../utils/catchAsync.js'
 import { createError } from '../utils/error.js'
 
 const signToken = id => {
@@ -61,3 +63,24 @@ export const login = async (req, res, next) => {
     next(err)
   }
 }
+
+export const protect = catchAsync(async (req, res, next) => {
+  let token
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1]
+  }
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401)
+    )
+  }
+
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT)
+  console.log(decoded)
+
+  next()
+})
