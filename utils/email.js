e2f637066d214419e213691e273f16a5828391f9
brianzenhom <brianzenhom@hotmail.com>
2024-03-13 00:14:18 +0100

    Refactor sendEmail function to use async/await and add nodemailer configuration

--- utils/email.js
+++ utils/email.js
@@ -1,7 +1,26 @@
 import nodemailer from 'nodemailer'
 
-const sendEmail = options => {
+const sendEmail = async options => {
   // Create a transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.NOREPLYHOST,
+    port: process.env.NOREPLYPORT,
+    auth: {
+      user: process.env.NOREPLYEMAIL,
+      pass: process.env.NOREPLYPW,
+    },
+  })
   // Define the email options
+  const mailOptions = {
+    from: 'Moon Valley Tours <noreply@moonvalleytours.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html:
+  }
+
   // Actually send the email
+  await transporter.sendMail(mailOptions)
 }
+
+export default sendEmail
