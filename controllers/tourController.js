219634cf04c48997188a7f86a441336e942181f5
brianzenhom <brianzenhom@hotmail.com>
2024-03-16 11:45:40 +0100

    Refactor tourController.js for filtering, sorting, field limiting, and pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -72,12 +72,44 @@ export const getAllTour = catchAsync(async (req, res, next) => {
   // Build query
   const queryObj = { ...req.query }
 
+  // Filtering
+
   const excludedFields = ['page', 'sort', 'limit', 'fields']
   excludedFields.forEach(el => delete queryObj[el])
 
-  const query = Tour.find(queryObj)
+  // Advanced filtering
+  let queryStr = JSON.stringify(queryObj)
+  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
 
-  // const query = Tour.find().where('duration').equals(5).where('difficulty').equals('easy')
+  let query = Tour.find(JSON.parse(queryStr))
+
+  // Sorting
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ')
+    query = query.sort(sortBy)
+  } else {
+    query = query.sort('-createdAt')
+  }
+
+  // Field Limiting
+  if (req.query.fields) {
+    const fields = req.query.fields.split(',').join(' ')
+    query = query.select(fields)
+  } else {
+    query = query.select('-__v')
+  }
+
+  // Pagination
+  const page = req.query.page * 1 || 1
+  const limit = req.query.limit * 1 || 100
+  const skip = (page - 1) * limit
+
+  query = query.skip(skip).limit(limit)
+
+  if (req.query.page) {
+    const numTours = await Tour.countDocuments()
+    if (skip >= numTours) throw new AppError('This page does not exist', 404)
+  }
 
   // Execute query
   const tours = await query
@@ -92,11 +124,13 @@ export const getAllTour = catchAsync(async (req, res, next) => {
 })
 
 export const getToursInCity = catchAsync(async (req, res, next) => {
+  const count = await Tour.find({ city: req.params.city }).count()
+
   const tours = await Tour.find({ city: req.params.city })
 
   if (!tours) {
     return next(new AppError('No tours found with that city', 404))
   }
 
-  res.status(200).json(tours)
+  res.status(200).json({ count, tours })
 })
