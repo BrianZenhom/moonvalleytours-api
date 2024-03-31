d5a5390638612917e5a3fcec85c5e5f75b8d25af
brianzenhom <brianzenhom@hotmail.com>
2024-03-31 03:45:48 +0200

    Refactor getTour function to remove unnecessary population

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -47,10 +47,7 @@ export const updateTour = catchAsync(async (req, res, next) => {
 })
 
 export const getTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id).populate({
-    path: 'guides',
-    select: '-__v -updatedAt -createdAt',
-  })
+  const tour = await Tour.findById(req.params.id)
 
   if (!tour) {
     return next(new AppError('No tour found with that ID', 404))
