f3ac1776f0a7200bcf474b06e4a74684c6befcec
brianzenhom <brianzenhom@hotmail.com>
2024-03-31 01:00:18 +0100

    Refactor getCountry function and populate cities

--- controllers/countryController.js
+++ controllers/countryController.js
@@ -11,13 +11,10 @@ export const createCountry = async (req, res, next) => {
   }
 }
 export const getCountry = async (req, res) => {
-  const query = req.query.q
-
   try {
     const country = await Countries.findOne({
       country: req.params.country,
-      query,
-    })
+    }).populate('cities')
     res.status(200).json(country)
   } catch (err) {
     next(err)
