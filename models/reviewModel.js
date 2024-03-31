a69f3ebf2d49c9c4e7f9d1520d1cc25835194a18
brianzenhom <brianzenhom@hotmail.com>
2024-03-31 03:56:45 +0200

    Add review and rating fields to ReviewSchema

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,3 +1,32 @@
 import mongoose from 'mongoose'
 
-const ReviewSchema = new mongoose.Schema({})
+const ReviewSchema = new mongoose.Schema({
+  // Review
+  review: { type: String, required: [true, 'Review cannot be emty!'] },
+  // Rating
+  rating: {
+    type: Number,
+    default: 5,
+    min: [1, 'Rating must be above 1'],
+    max: [5, 'Rating must be below 5'],
+  },
+  createdAt: { type: Date, default: Date.now },
+  // ref tour
+  tour: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour.'],
+    },
+  ],
+  // ref to user
+  user: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user'],
+    },
+  ],
+})
+
+export default mongoose.model('Review', ReviewSchema)
