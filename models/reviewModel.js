24a20f085972f05856b0701055c4a0f419bb5a87
brianzenhom <brianzenhom@hotmail.com>
2024-04-01 10:20:35 +0200

    Refactor ReviewSchema in reviewModel.js

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,28 +1,38 @@
 import mongoose from 'mongoose'
 
-const ReviewSchema = new mongoose.Schema({
-  review: { type: String, required: [true, 'Review cannot be emty!'] },
-  rating: {
-    type: Number,
-    default: 5,
-    min: [1, 'Rating must be above 1'],
-    max: [5, 'Rating must be below 5'],
+const ReviewSchema = new mongoose.Schema(
+  {
+    review: { type: String, required: [true, 'Review cannot be emty!'] },
+    rating: {
+      type: Number,
+      default: 5,
+      min: [1, 'Rating must be above 1'],
+      max: [5, 'Rating must be below 5'],
+    },
+    createdAt: { type: Date, default: Date.now },
+    tour: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Tour',
+        required: [true, 'Review must belong to a tour.'],
+      },
+    ],
+    user: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: [true, 'Review must belong to a user'],
+      },
+    ],
   },
-  createdAt: { type: Date, default: Date.now },
-  tour: [
-    {
-      type: mongoose.Schema.ObjectId,
-      ref: 'Tour',
-      required: [true, 'Review must belong to a tour.'],
+  {
+    toJSON: {
+      virtuals: true,
     },
-  ],
-  user: [
-    {
-      type: mongoose.Schema.ObjectId,
-      ref: 'User',
-      required: [true, 'Review must belong to a user'],
+    toObject: {
+      virtuals: true,
     },
-  ],
-})
+  }
+)
 
 export default mongoose.model('Review', ReviewSchema)
