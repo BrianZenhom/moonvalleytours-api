5c4d85d53b2a348b0947dadbe5c063498ce61d52
brianzenhom <brianzenhom@hotmail.com>
2024-04-02 11:39:46 +0200

    Add population of 'reviews' field in getTour function

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -44,7 +44,7 @@ export const updateTour = catchAsync(async (req, res, next) => {
 })
 
 export const getTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id)
+  const tour = await Tour.findById(req.params.id).populate('reviews')
 
   if (!tour) {
     return next(new AppError('No tour found with that ID', 404))
