c912ecd0b91e96ede9d3abedf09870734b0ed41b
brianzenhom <brianzenhom@hotmail.com>
2024-04-08 16:35:51 +0200

    Add nested route support and set default values in createReview function

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -13,6 +13,10 @@ export const getAllReviews = catchAsync(async (req, res, next) => {
 })
 
 export const createReview = catchAsync(async (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId
+  if (!req.body.user) req.body.user = req.user.id
+
   const newReview = await Review.create(req.body)
 
   res.status(200).json({
