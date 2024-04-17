3c18bf689d26f23aa215239df0117ee55b935f28
brianzenhom <brianzenhom@hotmail.com>
2024-04-17 21:40:45 +0200

    Add deleteOne Handler

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,6 @@
 import Review from '../models/reviewModel.js'
 import catchAsync from '../utils/catchAsync.js'
+import { deleteOne } from './handlerFactory.js'
 
 export const getAllReviews = catchAsync(async (req, res, next) => {
   let filter = {}
@@ -28,3 +29,5 @@ export const createReview = catchAsync(async (req, res, next) => {
     },
   })
 })
+
+export const deleteReview = deleteOne(Review)
