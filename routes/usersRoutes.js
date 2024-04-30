8ccebcedad02817190cbdb163169df01492f405b
brianzenhom <brianzenhom@hotmail.com>
2024-04-30 08:18:45 +0200

    Remove unecessary imports

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -3,13 +3,14 @@ import {
   updateUser,
   getUser,
   getAllUsers,
+  deleteUser,
 } from '../controllers/userController.js'
 import { protect, restrictTo } from '../controllers/authController.js'
 
 const router = express.Router()
 
 // Client routes
-router.route('/:id').get(getUser).put(updateUser)
+router.route('/:id').get(getUser).put(updateUser).delete(deleteUser)
 
 // Admin routes
 router.get('/', protect, restrictTo('admin', 'lead-guide'), getAllUsers)
