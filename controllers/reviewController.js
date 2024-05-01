3b378457ca3777741cc1d1d37d48f7f8bb9ca1cc
brianzenhom <brianzenhom@hotmail.com>
2024-05-01 08:47:05 +0200

    Add Middleware setTourUserIds

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,6 @@
 import Review from '../models/reviewModel.js'
 import catchAsync from '../utils/catchAsync.js'
-import { deleteOne, updateOne } from './handlerFactory.js'
+import { createOne, deleteOne, updateOne } from './handlerFactory.js'
 
 export const getAllReviews = catchAsync(async (req, res, next) => {
   let filter = {}
@@ -15,20 +15,15 @@ export const getAllReviews = catchAsync(async (req, res, next) => {
   })
 })
 
-export const createReview = catchAsync(async (req, res, next) => {
+export const setTourUserIds = (req, res, next) => {
   // Allow nested routes
   if (!req.body.tour) req.body.tour = req.params.tourId
   if (!req.body.user) req.body.user = req.user.id
 
-  const newReview = await Review.create(req.body)
+  next()
+}
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      newReview,
-    },
-  })
-})
+export const createReview = createOne(Review)
 
 export const updateReview = updateOne(Review)
 
