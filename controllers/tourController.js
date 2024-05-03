953f4b3cff1a23cd14fee676e2683597a764e977
brianzenhom <brianzenhom@hotmail.com>
2024-05-03 09:29:20 +0200

    Add getAll handlerFactory

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,7 @@ import Tour from '../models/tourModel.js'
 import City from '../models/cityModel.js'
 import catchAsync from '../utils/catchAsync.js'
 import AppError from '../utils/appError.js'
-import { deleteOne, getOne, updateOne } from './handlerFactory.js'
-import { APIFeatures } from '../utils/apiFeatures.js'
+import { deleteOne, getAll, getOne, updateOne } from './handlerFactory.js'
 
 export const aliasTopTours = (req, res, next) => {
   req.query.limit = '7'
@@ -15,15 +14,12 @@ export const aliasTopTours = (req, res, next) => {
 }
 
 export const createTour = catchAsync(async (req, res, next) => {
-  const city = req.params.city
+  const city = req.params.cityId
   const newTour = new Tour(req.body)
 
   const savedTour = await newTour.save()
 
-  await City.findOneAndUpdate(
-    { city: city },
-    { $push: { tours: savedTour._id } }
-  )
+  await City.findByIdAndUpdate(city, { $push: { tours: savedTour._id } })
 
   res.status(200).json({
     status: 'success',
@@ -36,25 +32,7 @@ export const createTour = catchAsync(async (req, res, next) => {
 export const getTour = getOne(Tour, { path: 'reviews' })
 export const updateTour = updateOne(Tour)
 export const deleteTour = deleteOne(Tour)
-
-export const getAllTour = catchAsync(async (req, res, next) => {
-  // Execute query
-  const features = new APIFeatures(Tour.find(), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate()
-
-  const tours = await features.query
-
-  res.status(200).json({
-    status: 'success',
-    count: tours.length,
-    data: {
-      tours,
-    },
-  })
-})
+export const getAllTour = getAll(Tour)
 
 export const getToursInCity = catchAsync(async (req, res, next) => {
   const tours = await Tour.find({ city: req.params.city })
