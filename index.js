2a08ed688dfc171aa923b1e526b7be64cf60193a
brianzenhom <brianzenhom@hotmail.com>
2024-05-04 11:04:45 +0200

    test

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import usersRoute from './routes/usersRoutes.js'
 import reviewRoutes from './routes/reviewRoutes.js'
 
 import cookieParser from 'cookie-parser'
-// import cors from 'cors'
+import cors from 'cors'
 
 import AppError from './utils/appError.js'
 import globalErrorHandler from './controllers/errorController.js'
@@ -43,18 +43,10 @@ mongoose.connection.on('disconnected', () => {
 })
 
 // Set cors
-// app.use(
-//   cors({
-//     origin: [
-//       'http://localhost:5173',
-//       'http://localhost:3000',
-//       'http://localhost:5174',
-//       'https://moonvalleytours.com',
-//       'https://moonvalleytours-api.vercel.app',
-//     ],
-//     credentials: true,
-//   })
-// )
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 // // Limit Request from same IP
 // const limiter = rateLimit({
@@ -65,7 +57,6 @@ mongoose.connection.on('disconnected', () => {
 // app.use('/api', limiter)
 
 // Body parser
-app.use(cookieParser())
 
 // app.use(express.json({ limit: '50mb' }))
 // app.use(express.urlencoded({ extended: true, limit: '50mb' }))
