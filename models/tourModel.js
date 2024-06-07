4f5a5a741c9666438307051173ecba23da77302a
brianzenhom <brianzenhom@hotmail.com>
2024-06-08 00:44:30 +0200

    Add Goespatial queries

--- models/tourModel.js
+++ models/tourModel.js
@@ -65,6 +65,7 @@ const TourSchema = new mongoose.Schema(
       default: 5,
       min: [1, 'Rating must be above 1.0'],
       max: [5, 'Rating must be below 5.0'],
+      set: val => Math.round(val * 10) / 10,
     },
     ratingsQuantity: { type: Number, default: 0 },
     travellers: { type: Number, default: 0, min: 0 },
