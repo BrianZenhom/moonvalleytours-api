f1869912ed11303ff30972db3615351104b181f0
brianzenhom <brianzenhom@hotmail.com>
2024-06-08 05:58:07 +0200

    Fix Routes

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -129,3 +129,47 @@ export const getToursWithin = catchAsync(async (req, res, next) => {
     },
   })
 })
+
+export const getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params
+  const [lat, lng] = latlng.split(',')
+
+  const multiplier = unit === 'mi' ? 0.000621371 : 0.001
+
+  if (!lat || !lng) {
+    next(
+      new AppError(
+        'Please provide latitude and longitude in the format lat,lng.',
+        400
+      )
+    )
+  }
+
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1],
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multiplier,
+      },
+    },
+    {
+      $project: {
+        distance: 1,
+        title: 1,
+        _id: 1,
+        tourThumbnail: 1,
+      },
+    },
+  ])
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: distances,
+    },
+  })
+})
