2307d221fa16d018f0f44319e1368255e85532b9
brianzenhom <brianzenhom@hotmail.com>
2024-06-11 23:19:17 +0200

    Remove Comments

--- models/userModel.js
+++ models/userModel.js
@@ -53,13 +53,10 @@ const UserSchema = new mongoose.Schema(
 )
 
 UserSchema.pre('save', async function (next) {
-  // Only run this function if password was actually modified
   if (!this.isModified('password')) return next()
 
-  // Hash the password with cost of 12
   this.password = await bcrypt.hash(this.password, 12)
 
-  // Delete passwordConfirm field
   this.passwordConfirm = undefined
 
   next()
