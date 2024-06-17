de72f4487a75fadf60c7fb7a25b08a071f7cc8c9
brianzenhom <brianzenhom@hotmail.com>
2024-06-17 13:26:50 +0200

    Fix eslintIssue

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -6,7 +6,7 @@ import { getAll, getOne, updateOne } from './handlerFactory.js'
 export const getCity = getOne(City, {
   path: 'tours',
   select:
-    'title price priceDiscount duration ratingsAverage tourThumbnail cancellation country city slug',
+    'title price priceDiscount duration ratingsAverage tourThumbnail cancellation country city slug'
 })
 
 export const updateCity = updateOne(City)
@@ -15,7 +15,7 @@ export const deleteCity = catchAsync(async (req, res, next) => {
   await City.findByIdAndDelete(req.params.cityId)
 
   await Country.findByIdAndUpdate(req.params.countryId, {
-    $pull: { cities: req.params.cityId },
+    $pull: { cities: req.params.cityId }
   })
 
   res.status(200).json('City deleted!')
@@ -33,8 +33,8 @@ export const getCitiesInCountry = catchAsync(async (req, res, next) => {
     status: 'success',
     count: cities.length,
     data: {
-      cities,
-    },
+      cities
+    }
   })
 })
 
@@ -44,13 +44,13 @@ export const createCityInCountry = catchAsync(async (req, res, next) => {
   const newCity = await City.create(req.body)
 
   await Country.findByIdAndUpdate(req.params.countryId, {
-    $push: { cities: newCity._id },
+    $push: { cities: newCity._id }
   })
 
   res.status(200).json({
     status: 'success',
     data: {
-      newCity,
-    },
+      newCity
+    }
   })
 })
