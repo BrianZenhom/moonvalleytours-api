ae3e19aca103df5d8312b17a1a5b25036ec6ad66
brianzenhom <brianzenhom@hotmail.com>
2024-06-17 13:27:37 +0200

    Fix eslintIssue

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import { getOverview, getTour } from '../controllers/viewsController'
+import { getLoginForm, getTour } from '../controllers/viewsController'
 
 const router = express.Router()
 
 router.get('/tour/:slug', getTour)
 
+// Login
+router.get('/login', getLoginForm)
+
 export default router
-
-
-
