68511330c49851392e053b24af00e1433987eaa7
brianzenhom <brianzenhom@hotmail.com>
2024-06-17 13:16:20 +0200

    Add pug views templates

---
+++ controllers/viewsController.js
@@ -0,0 +1,23 @@
+import Tour from '../models/tourModel'
+import catchAsync from '../utils/catchAsync'
+
+export const getOverview = catchAsync(async (req, res, next) => {
+    const tours = await Tour.find()
+
+    res.status(200).render('overview', {
+        title: 'All tours',
+        tours
+    })
+})
+
+export const getTour = catchAsync(async (req, res, next) => {
+
+    const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+        path: 'reviews',
+        fields: 'review rating user'
+    })
+    
+    res.status(200).render('tour', {
+        tour
+    })
+})
