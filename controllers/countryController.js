ca65237b0e7a4a26f07e4ba06157de9807d896c8
brianzenhom <brianzenhom@hotmail.com>
2024-06-17 13:26:59 +0200

    Fix eslintIssue

--- controllers/countryController.js
+++ controllers/countryController.js
@@ -26,7 +26,7 @@ export const getAllCountries = catchAsync(async (req, res, next) => {
   res.status(200).json({
     status: 'success',
     data: {
-      countries,
-    },
+      countries
+    }
   })
 })
