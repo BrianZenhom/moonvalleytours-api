93660180285945c4cb2c970604d6a8004db6b8f2
brianzenhom <brianzenhom@hotmail.com>
2024-06-17 13:27:12 +0200

    Fix eslintIssue

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -2,22 +2,25 @@ import Tour from '../models/tourModel'
 import catchAsync from '../utils/catchAsync'
 
 export const getOverview = catchAsync(async (req, res, next) => {
-    const tours = await Tour.find()
+  const tours = await Tour.find()
 
-    res.status(200).render('overview', {
-        title: 'All tours',
-        tours
-    })
+  res.status(200).render('overview', {
+    title: 'All tours',
+    tours
+  })
 })
 
 export const getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
+  })
 
-    const tour = await Tour.findOne({ slug: req.params.slug }).populate({
-        path: 'reviews',
-        fields: 'review rating user'
-    })
-    
-    res.status(200).render('tour', {
-        tour
-    })
+  res.status(200).render('tour', {
+    tour
+  })
 })
+
+export const getLoginForm = (req, res) => {
+
+}
