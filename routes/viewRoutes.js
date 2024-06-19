a9a50a6e69dd5e8e448cfa8c9ed12cbc33f30cc8
brianzenhom <brianzenhom@hotmail.com>
2024-06-19 11:21:49 +0200

    Add login

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { getLoginForm, getOverview, getTour } from '../controllers/viewsController.js'
+import { protect } from '../controllers/authController.js'
 
 const router = express.Router()
 
-router.get('/', getOverview)
+router.get('/', protect, getOverview)
 
 router.get('/tour/:slug', getTour)
 
