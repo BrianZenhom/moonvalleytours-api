6a79fcab14bca227e15ceec0f9bc39b34a3e8850
brianzenhom <brianzenhom@hotmail.com>
2024-06-25 01:47:52 +0200

    Add bundler webpack

---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+import { login } from './login'
+
+document.querySelector('.form').addEventListener('submit', e => {
+  e.preventDefault()
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+  login(email, password)
+})
