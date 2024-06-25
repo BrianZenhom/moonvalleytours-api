481520029bcd1fe9e3176842293263986adb8ed1
brianzenhom <brianzenhom@hotmail.com>
2024-06-25 17:12:09 +0200

    Add 404 template pages

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,6 @@
 import Tour from '../models/tourModel.js'
 import catchAsync from '../utils/catchAsync.js'
+import AppError from '../utils/appError.js'
 
 export const getOverview = catchAsync(async (req, res, next) => {
   const tours = await Tour.find()
@@ -15,6 +16,10 @@ export const getTour = catchAsync(async (req, res, next) => {
     path: 'reviews',
     fields: 'review rating user'
   })
+
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404))
+  }
 
   res.status(200).render('tour', {
     tour
