59665ac90e2c2fe40daefbc7fe7fcd952fd21336
brianzenhom <brianzenhom@hotmail.com>
2024-06-25 17:12:56 +0200

    Add login alerts

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,19 @@
-import { login } from './login'
+import '@babel/polyfill'
+import { login, logout } from './login'
 
-document.querySelector('.form').addEventListener('submit', e => {
-  e.preventDefault()
-  const email = document.getElementById('email').value
-  const password = document.getElementById('password').value
-  login(email, password)
-})
+// DOM ELEMENTS
+const loginForm = document.querySelector('.form')
+const logoutBtn = document.querySelector('.nav__el--logout')
+
+if (loginForm) {
+  loginForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    login(email, password)
+  })
+}
+
+if (logoutBtn) {
+  logoutBtn.addEventListener('click', logout)
+}
