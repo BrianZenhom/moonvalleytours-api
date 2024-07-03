03fbe1e339ff86a2b5d94a4cd1d4a71beaff6bbb
brianzenhom <brianzenhom@hotmail.com>
2024-07-03 03:45:56 +0200

    Fix middleware endpoints

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -12,10 +12,10 @@ import { protect, restrictTo } from '../controllers/authController.js'
 
 const router = express.Router()
 
-router.use(protect)
-
-router.get('/me', getMe, getUser)
 router.patch('/updateMe', updateMe)
+
+router.use(protect)
+router.get('/me', getMe, getUser)
 router.delete('/deleteMe', deleteMe)
 
 router.use(restrictTo('admin'))
