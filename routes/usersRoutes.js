542f8dabd8a2f2a14f59cacd94a284fb0c75ceec
brianzenhom <brianzenhom@hotmail.com>
2024-07-09 03:11:28 +0200

    Add multer

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -9,16 +9,19 @@ import {
   getMe
 } from '../controllers/userController.js'
 import { protect, restrictTo } from '../controllers/authController.js'
+import multer from 'multer'
+
+const upload = multer({ dest: 'public/img/users' })
 
 const router = express.Router()
 
 router.use(protect)
-router.patch('/updateMe', updateMe)
 router.get('/me', getMe, getUser)
+router.patch('/updateMe', upload.single('photo'), updateMe)
 router.delete('/deleteMe', deleteMe)
 
 router.use(restrictTo('admin'))
-router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
 router.get('/', restrictTo('admin', 'lead-guide'), getAllUsers)
+router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
 
 export default router
