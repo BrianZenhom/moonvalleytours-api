7653b6c047db5fb1a75ddcb3954d160d749ebbee
brianzenhom <brianzenhom@hotmail.com>
2024-07-11 02:42:04 +0200

    Add uploadUserPhoto and resizeUserPhoto

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,11 @@
 import Users from '../models/userModel.js'
+import sharp from 'sharp'
 import AppError from '../utils/appError.js'
 import catchAsync from '../utils/catchAsync.js'
 import multer from 'multer'
 import { deleteOne, getAll, getOne, updateOne } from './handlerFactory.js'
 
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/img/users')
-  },
-  filename: (req, file, cb) => {
-    // user-:id-:currtimestamp.jpg
-    const ext = file.mimetype.split('/')[1]
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`)
-  }
-
-})
+const multerStorage = multer.memoryStorage()
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
@@ -30,6 +21,20 @@ export const upload = multer({
 })
 
 export const uploadUserPhoto = upload.single('photo')
+
+export const resizeUserPhoto = (req, res, next) => {
+  if (!req.file) return next()
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`
+
+  sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`)
+
+  next()
+}
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {}
