223c6bb332f9bdee153a5ccba03cb6a3d05bd5cc
brianzenhom <brianzenhom@hotmail.com>
2024-07-18 21:09:31 +0200

    Add tour photos

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,19 +22,19 @@ export const upload = multer({
 
 export const uploadUserPhoto = upload.single('photo')
 
-export const resizeUserPhoto = (req, res, next) => {
+export const resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next()
 
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`
 
-  sharp(req.file.buffer)
+  await sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
     .toFile(`public/img/users/${req.file.filename}`)
 
   next()
-}
+})
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {}
