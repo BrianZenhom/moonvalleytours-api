fd178836a59f07a36cf441d0d48a550a1aea53e9
brianzenhom <brianzenhom@hotmail.com>
2024-09-24 08:31:24 +0200

    Add stripe

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -3,7 +3,6 @@ import { getCheckoutSession } from './../controllers/bookingController.js'
 import { protect } from './../controllers/authController.js'
 
 const router = express.Router()
-
-router.get('/checkout-session/:tourID', protect, getCheckoutSession)
+router.post('/checkout-session/:tourID', protect, getCheckoutSession)
 
 export default router
